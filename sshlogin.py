73d820cb8d76ea40c466f57aa29b858620f470be
biba7bg <147879963+biba7bg@users.noreply.github.com>
2023-10-14 22:00:17 +0100

    ssh_codv_update1
    indent in menu choices fixed, login still doesn't work

--- sshlogin.py
+++ sshlogin.py
@@ -4,9 +4,10 @@ import subprocess
 import time
 import logging
 
+
 def get_credentials():  # creating ceredentials function
-    username = "bilayanabily" #input("Enter your username:"4
-    password = "password"  #getpass.getpass("Enter your password: ")
+    username = "bilyanabily"  # input("Enter your username:"4
+    password = "password"  # getpass.getpass("Enter your password: ")
     return username, password
 
 
@@ -22,32 +23,37 @@ def execute_commands(ssh):  # defining menu and choices function
 
         choice = input("Enter your choice (1/2/3/4/5/6): ")
         if choice == "1":
-            stdin, stdout, stderr = ssh.exec_command("systemctl --type=service")
+            stdin, stdout, stderr = ssh.exec_command(
+                "systemctl --type=service")
             print(stdout.read().decode())
         elif choice == "2":
-            stdin, stdout, stderr = ssh.exec_command("uptime && last reboot | head -n 5")
+            stdin, stdout, stderr = ssh.exec_command(
+                "uptime && last reboot | head -n 5")
             print(stdout.read().decode())
         elif choice == "3":
-            print("not ready yet")#stdin, stdout, stderr = ssh.exec_command("pgrep -f 'jvm_name'")
+            # stdin, stdout, stderr = ssh.exec_command("pgrep -f 'jvm_name'")
+            print("not ready yet")
             # old_pid = stdout.read().decode()strip()
             # print(f"old JVM OID: {old_pid}")
         elif choice == "4":
-            confirm = input("Are you sure you want to restart the server? (yes/no): ")
+            confirm = input(
+                "Are you sure you want to restart the server? (yes/no): ")
             if confirm.lower() == 'yes':
                 ssh.exec_command("sudo systemctl reboot")
                 # here I would like to add funtion to catch what happens at the background
                 print(" Server is rebooting...")
-                break
-            elif choice == "5":
-                print("not ready yet")
-            elif choice == "6":
-                print("Exiting...")
-                break
-            else:
-                print("Invalid choice, please try again")
+                exit(0)
+        elif choice == "5":
+            print("not ready yet")
+        elif choice == "6":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice, please try again")
+
 
 def main():
-    hostname =  "192.168.1.126"    #input(" Enter the server ip or host name: ")
+    hostname = "192.168.1.126"  # input(" Enter the server ip or host name: ")
     username, password = get_credentials()
     try:
         ssh = paramiko.SSHClient()
@@ -63,7 +69,6 @@ def main():
         print(f"Error occured: {str(e)}")
     finally:
         ssh.close
-
 
 
 if __name__ == "__main__":
