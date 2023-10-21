0c49d33f970d057a0037a7e310b1a429d7ad2f3f
biba7bg <147879963+biba7bg@users.noreply.github.com>
2023-10-21 21:57:02 +0100

    server menu function
    added server menu function

--- main.py
+++ main.py
@@ -26,7 +26,23 @@ def main_menu():
 
 
 def server_os():
-    print("I am server os choice function, and I am under construction")
+    # print("I am server os choice function, and I am under construction")
+    print("Choose server type:\n1. Linux\n2. Windows\n3. Return\n4. Quit")
+    choice = input(" Enter the server type:")
+    if choice == "1":
+        print("New function, use library for linux ")
+        linux_server_menu()
+    elif choice == "2":
+        print("New function, use library for windows ")
+        windows_server_menu()
+    elif choice == "3":
+        return main_menu()
+    elif choice == "4":
+        exit(0)
+    else:
+        logging.error("Invalid server menu")
+        print(" Invalid os choice")
+    return int(choice)
 
 
 def linux_server_menu():
--- menuoptions.py
+++ menuoptions.py
@@ -1,2 +1,30 @@
 import subprocess
+import getpass
+import subprocess
+import manues
 
+
+def get_credentials():  # creating credentials function
+    username = "bilyana"  # input("Enter your username:"4
+    password = "pa55word"  # getpass.getpass("Enter your password: ")
+    return username, password
+
+
+# functions for server options
+
+def linux_server_reboot():
+    print("I am linux reboot function")
+
+
+def windows_server_reboot():
+    print("I am windows reboot function")
+
+# Functions for services options
+
+
+def linux_allservices():
+    print("I am linux server all services dislay function")
+
+
+def linux_JVM():
+    print("I am linux JVM restart function")
